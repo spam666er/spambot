be587227993870e95949d8e42b7761535fa705e0
Marcin <marcinberus@hotmail.com>
2018-02-08 18:59:38 +0100

    data provider with recepients

--- src/test/java/SpamSender/SpamSender.java
+++ src/test/java/SpamSender/SpamSender.java
@@ -4,8 +4,9 @@ import Google.GoogleMainPage;
 import Utilities.Utils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class SpamSender {
@@ -13,7 +14,7 @@ public class SpamSender {
     private WebDriver driver;
     private GoogleMainPage googleMainPage;
 
-    @BeforeTest
+    @BeforeMethod
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "C:\\Tools\\chromedriver.exe");
         driver = new ChromeDriver();
@@ -23,8 +24,16 @@ public class SpamSender {
         googleMainPage = new GoogleMainPage(driver);
     }
 
-    @Test
-    public void SpamSenderTest() {
+    @DataProvider(name = "emailsList")
+    public static Object[][] emailsList() {
+        return new Object[][] {
+                {"edwarddziaslo123@gmail.com"},
+                {"spam666catcher@gmail.com"}
+        };
+    }
+
+    @Test(dataProvider = "emailsList")
+    public void SpamSenderTest(String email) {
         googleMainPage
                 .clickOnGmailMailboxbutton()
                 .clickOnSignInButton()
@@ -35,13 +44,13 @@ public class SpamSender {
                 .clickNextButtonAfterPassword()
 
                 .clickOnComposeButton()
-                .enterRecepient("spam666catcher@gmail.com")
+                .enterRecepient(email)
                 .enterSubject(Utils.getRandomData("titles"))
                 .enterMessage(Utils.getRandomData("messages"))
                 .clickSendMessage();
     }
 
-    @AfterTest
+    @AfterMethod
     public void tearDown() {
         driver.quit();
     }
